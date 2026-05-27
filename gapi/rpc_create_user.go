622e79ae76813da6eeeb16461ba71c20e34ace1e
Yuttana Khumnual <yuttanakhumnual@MacBook-Air-khxngYuttana.local>
2026-05-28 02:29:53 +0700

    Finish on lecture 56

--- gapi/rpc_create_user.go
+++ gapi/rpc_create_user.go
@@ -44,6 +44,7 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err)
 	}
 
+	// TODO: send verify email
 	rsp := &pb.CreateUserResponse{
 		User: convertUser(user),
 	}
