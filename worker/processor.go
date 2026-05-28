1704b8d94500649334a13a5b4b6e8dd3752f7013
Yuttana Khumnual <yuttanakhumnual@MacBook-Air-khxngYuttana.local>
2026-05-29 04:26:07 +0700

    Finish on 59

--- worker/processor.go
+++ worker/processor.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/hibiken/asynq"
+	"github.com/rs/zerolog/log"
 
 	db "github.com/yuttana76/simbplebank/db/sqlc"
 )
@@ -31,6 +32,10 @@ func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskPr
 				QueueCritical: 10,
 				QueueDefault:  5,
 			},
+			ErrorHandler: asynq.ErrorHandlerFunc(func(ctx context.Context, task *asynq.Task, err error) {
+				log.Error().Err(err).Str("type", task.Type()).Bytes("payload", task.Payload()).Msg("Process task failed")
+			}),
+			Logger: NewLogger(),
 		},
 	)
 
