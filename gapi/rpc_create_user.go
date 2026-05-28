25d75e71c09b5f58ae847509a46c3a5bdca877a2
Yuttana Khumnual <yuttanakhumnual@MacBook-Air-khxngYuttana.local>
2026-05-29 03:51:46 +0700

    Finish on 59. How to handle errors and print logs for Async workers

--- gapi/rpc_create_user.go
+++ gapi/rpc_create_user.go
@@ -28,17 +28,30 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 		return nil, status.Errorf(codes.Internal, "failed to hash password:%s", err)
 	}
 
-	arg := db.CreateUserParams{
-		Username:       req.GetUsername(),
-		HashedPassword: hashedPassword,
-		FullName:       req.GetFullName(),
-		Email:          req.GetEmail(),
+	arg := db.CreateUserTxParams{
+		CreateUserParams: db.CreateUserParams{
+			Username:       req.GetUsername(),
+			HashedPassword: hashedPassword,
+			FullName:       req.GetFullName(),
+			Email:          req.GetEmail(),
+		},
+		AfterCreate: func(user db.User) error {
+			//Send verify email background worker
+			taskPayload := &worker.PayloadSendVerifyEmail{
+				Username: user.Username,
+			}
+			opts := []asynq.Option{
+				asynq.MaxRetry(10),                // max retry 10 times
+				asynq.ProcessIn(10 * time.Second), // process in 10 seconds
+				asynq.Queue(worker.QueueCritical), // queue critical
+			}
+			return server.taskDistributor.DistributeTaskSendVerifyEmail(ctx, taskPayload, opts...)
+		},
 	}
 
-	user, err := server.store.CreateUser(ctx, arg)
+	txResult, err := server.store.CreateUserTx(ctx, arg)
 	if err != nil {
 		if pgErr, ok := err.(*pq.Error); ok {
-			// log.Println(pgErr.Code.Name())
 			switch pgErr.Code.Name() {
 			case "unique_violation":
 				return nil, status.Errorf(codes.AlreadyExists, "username already exists: %s", err)
@@ -47,23 +60,8 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err)
 	}
 
-	//TODO: use db transaction
-	//Send verify email background worker
-	taskPayload := &worker.PayloadSendVerifyEmail{
-		Username: user.Username,
-	}
-	opts := []asynq.Option{
-		asynq.MaxRetry(10),                // max retry 10 times
-		asynq.ProcessIn(10 * time.Second), // process in 10 seconds
-		asynq.Queue(worker.QueueCritical), // queue critical
-	}
-	err = server.taskDistributor.DistributeTaskSendVerifyEmail(ctx, taskPayload, opts...)
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "failed to distribute task to send verify email: %s", err)
-	}
-
 	rsp := &pb.CreateUserResponse{
-		User: convertUser(user),
+		User: convertUser(txResult.User),
 	}
 	return rsp, nil
 }
