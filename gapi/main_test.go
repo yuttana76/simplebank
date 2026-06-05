abe8b420cafa4910eea50cafc0d5f1dcac185721
Yuttana Khumnual <yuttanakhumnual@AirkhxngYuttana.true.true>
2026-06-06 03:34:44 +0700

    Finish on lecture 66 How to test a gRPC API that requires authentication

--- gapi/main_test.go
+++ gapi/main_test.go
@@ -1,13 +1,17 @@
 package gapi
 
 import (
+	"context"
+	"fmt"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
 	db "github.com/yuttana76/simbplebank/db/sqlc"
+	"github.com/yuttana76/simbplebank/token"
 	"github.com/yuttana76/simbplebank/util"
 	"github.com/yuttana76/simbplebank/worker"
+	"google.golang.org/grpc/metadata"
 )
 
 func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDistributor) *Server {
@@ -20,4 +24,16 @@ func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDist
 	require.NoError(t, err)
 
 	return server
+}
+
+func newContextWithBearerToken(t *testing.T, tokenMaker token.Maker, username string, duration time.Duration) context.Context {
+
+	role := util.DepositorRole
+	accessToken, _, err := tokenMaker.CreateToken(username, role, duration, token.TokenTypeAccessToken)
+	require.NoError(t, err)
+	bearerToken := fmt.Sprintf("%s %s", authoriaztionBearer, accessToken)
+	md := metadata.MD{
+		authoriaztionHeader: []string{bearerToken},
+	}
+	return metadata.NewIncomingContext(context.Background(), md)
 }
